585a5521b0d4b8e0ac5e2964c9523ade37921439
animka0433 <animka0433@gmail.com>
2025-12-08 17:01:02 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,30 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=False)
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+class Question(Base):
+    __tablename__ = "questions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    question = Column(String)
+    option1 = Column(String)
+    option2 = Column(String)
+    option3 = Column(String)
+    option4 = Column(String)
+    correct_option = Column(Integer)
+
+class UserQuizSession(Base):
+    __tablename__ = "user_quiz_session"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    score = Column(Integer)
