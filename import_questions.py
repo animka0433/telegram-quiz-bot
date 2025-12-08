d7a2586e3db199dce75cf14bb1fe08769bfee3f3
animka0433 <animka0433@gmail.com>
2025-12-08 17:01:44 +0530

    Update import_questions.py

--- import_questions.py
+++ import_questions.py
@@ -1,1 +1,21 @@
+import ujson as json
+from models import SessionLocal, Question
+
+session = SessionLocal()
+
+file = "sample_questions.json"
+data = json.load(open(file))
+
+for q in data:
+    session.add(Question(
+        question=q["question"],
+        option1=q["options"][0],
+        option2=q["options"][1],
+        option3=q["options"][2],
+        option4=q["options"][3],
+        correct_option=q["answer"],
+    ))
+
+session.commit()
+print("Imported", len(data), "questions!")
 
