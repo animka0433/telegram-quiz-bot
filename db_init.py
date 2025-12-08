4efb1943170597d02cf4236b8e7ded4d2f4b6924
animka0433 <animka0433@gmail.com>
2025-12-08 17:01:17 +0530

    Update db_init.py

--- db_init.py
+++ db_init.py
@@ -1,1 +1,25 @@
+import ujson as json
+from models import Base, engine, SessionLocal, Question
+
+print("📌 Creating tables...")
+Base.metadata.create_all(bind=engine)
+
+session = SessionLocal()
+
+with open("sample_questions.json", "r", encoding="utf8") as f:
+    data = json.load(f)
+
+print("📌 Importing sample questions...")
+for q in data:
+    session.add(Question(
+        question=q["question"],
+        option1=q["options"][0],
+        option2=q["options"][1],
+        option3=q["options"][2],
+        option4=q["options"][3],
+        correct_option=q["answer"]
+    ))
+
+session.commit()
+print("✅ Database ready!")
 
